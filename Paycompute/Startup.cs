9b26314623bc312588c94189dbf363244b4b14eb
Mario Zelaya <mariozelaya23@hotmail.com>
2020-06-10 10:42:13 -0700

    Layout, Index-EmployeeController

--- Paycompute/Startup.cs
+++ Paycompute/Startup.cs
@@ -12,6 +12,9 @@ using Paycompute.Persistence;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Paycompute.Services;
+using Paycompute.Services.Implementation;
+using Microsoft.AspNetCore.Http;
 
 namespace Paycompute
 {
@@ -27,6 +30,14 @@ namespace Paycompute
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential 
+                // cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                // requires using Microsoft.AspNetCore.Http;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -34,6 +45,7 @@ namespace Paycompute
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddScoped<IEmployeeService, EmployeeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +64,7 @@ namespace Paycompute
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
