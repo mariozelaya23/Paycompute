b921bce60479082dff92a6eb204896aa89735371
Mario Zelaya <mariozelaya23@hotmail.com>
2020-06-14 09:52:06 -0700

    SSNContributionService

---
+++ Paycompute.Services/Implementation/SSNContributionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Services.Implementation
+{
+    public class SSNContributionService : ISSNContributionService
+    {
+        private decimal SSNRate;
+        private decimal SSNC;
+        public decimal SSNContribution(decimal totalAmount)
+        {
+            if (totalAmount > 0)
+            {
+                SSNRate = .062m;
+                SSNC = (totalAmount * SSNRate);
+            }
+            return SSNC;
+        }
+    }
+}
