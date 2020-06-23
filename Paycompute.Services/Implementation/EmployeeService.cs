13d6a18285a3f17eb1c38886760e4507eed32b20
Mario Zelaya <mariozelaya23@hotmail.com>
2020-06-23 09:26:54 -0700

    Pagination of Employee List

--- Paycompute.Services/Implementation/EmployeeService.cs
+++ Paycompute.Services/Implementation/EmployeeService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Serialization;
 using Paycompute.Entity;
 using Paycompute.Persistence;
@@ -37,7 +38,7 @@ namespace Paycompute.Services.Implementation
             await _context.SaveChangesAsync();
         }
 
-        public IEnumerable<Employee> GetAll() => _context.Employees;
+        public IEnumerable<Employee> GetAll() => _context.Employees.AsNoTracking().OrderBy(emp => emp.FullName);
 
         public async Task UpdateAsync(Employee employee)
         {
